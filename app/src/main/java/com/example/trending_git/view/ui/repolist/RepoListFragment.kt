251dd14070d2d88fb400cd28f52f0bf985b04600
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-06-10 13:08:28 +0300

    Create RepoListFragment

---
+++ app/src/main/java/com/example/trending_git/view/ui/repolist/RepoListFragment.kt
@@ -0,0 +1,56 @@
+package com.example.trending_git.view.ui.repolist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.jetbrains.anko.longToast
+
+class RepoListFragment : Fragment() {
+
+    private lateinit var viewDataBinding: FragmentRepoListBinding
+    private lateinit var adapter: RepoListAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewDataBinding = FragmentRepoListBinding.inflate(inflater, container, false).apply{
+            viewModel = ViewModelProviders.of(this@RepoListFragment).get(RepoListViewModel::class.java)
+            setLifeCycleOwner(viewLifecycleOwner)
+        }
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewDataBinding.viewmodel?.fetchRepoList()
+
+        setupAdapter()
+        setupObservers()
+    }
+
+    private fun setupObservers(){
+        viewDataBinding.viewmodel?.repoListLive?.observe(viewLifecycleOwner, Observer {
+            adapter.updateRepoList(it)
+        })
+
+        viewDataBinding.viewmodel?.toastMessage?.observe(viewLifecycleOwner, Observer{
+            activity?.longToast(it)
+        })
+    }
+
+    private fun setupAdapter() {
+        val viewModel = viewDataBinding.viewmodel
+        if (viewModel != null) {
+            adapter = RepoListAdapter(viewDataBinding.viewModel!!)
+            val layoutManager = LinearLayoutManager(activity)
+            repo_list_rv.layoutManager = layoutManager
+            repo_list_rv.addItemDecoration(DividerItemDecoration(activity, layoutManager.orientation))
+            repo_list_rv.adapter = adapter
+        }
+    }
+}
