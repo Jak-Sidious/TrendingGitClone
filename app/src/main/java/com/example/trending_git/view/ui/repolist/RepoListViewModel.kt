a0949e1f574cd4b3f56ba98a1d8032954e4bd7b5
Jakana Kiwanuka <k.jak86@yahoo.com>
2019-06-10 15:35:32 +0300

    Create RepoListViewModel

---
+++ app/src/main/java/com/example/trending_git/view/ui/repolist/RepoListViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.trending_git.view.ui.repolist
+
+import android.content.ClipData
+import androidx.lifecycle.MutableLiveData
+import com.example.trending_git.view.base.BaseViewModel
+
+class RepoListViewModel : BaseViewModel(){
+
+    val repoListLive = MutableLiveData<List<ClipData.Item>>()
+
+    fun fetchRepoList() {
+        dataLoading.value = true
+        RepoRepository.getInstance().getRepoList { isSuccess, response ->
+            dataLoading.value = false
+            if (isSuccess) {
+                repoListLive.value = response?.items
+                empty.value = false
+            } else {
+                empty.value = true
+            }
+        }
+    }
+}
